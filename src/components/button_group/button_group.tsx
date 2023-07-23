bd8207193208ca62f952aafac7a8f08b566a6848
Bree Hall <briannajdhall@gmail.com>
2023-07-23 12:31:51 -0400

    [ButtonGroup] Created ButtonGroup component

---
+++ src/components/button_group/button_group.tsx
@@ -0,0 +1,58 @@
+import { FunctionComponent, useState } from 'react';
+import './button_group.scss';
+
+export type ButtonGroupItem = {
+  id: string;
+  label: string;
+  value: string | number;
+};
+
+export type ButtonGroupProps = {
+  items: ButtonGroupItem[];
+  defaultSelectedId?: string;
+  label: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+export const ButtonGroup: FunctionComponent<ButtonGroupProps> = ({
+  items,
+  defaultSelectedId,
+  label,
+  onClick,
+}) => {
+  const [selectedId, setSelectedId] = useState(defaultSelectedId);
+
+  const handleButtonOnClick = (e) => {
+    onClick(e);
+    setSelectedId(e.target.id);
+  };
+  return (
+    <>
+      <fieldset>
+        <legend className="buttonGroup__label">{label}</legend>
+        <div className="buttonGroup__container">
+          {items.map((button: ButtonGroupItem, key) => {
+            const { id, label, value } = button;
+
+            const classNames =
+              id === selectedId
+                ? 'buttonGroup__button--isSelected'
+                : 'buttonGroup__button';
+
+            return (
+              <button
+                className={classNames}
+                value={value}
+                onClick={handleButtonOnClick}
+                id={id}
+                key={key}
+              >
+                {label}
+              </button>
+            );
+          })}
+        </div>
+      </fieldset>
+    </>
+  );
+};
