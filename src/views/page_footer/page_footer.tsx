5cb9358b9dba432e1cdb45c0cd8c84b7d27bd49f
Bree Hall <briannajdhall@gmail.com>
2023-08-01 22:21:18 -0400

    Add disclaimer to footer

--- src/views/page_footer/page_footer.tsx
+++ src/views/page_footer/page_footer.tsx
@@ -1,5 +1,13 @@
 import './page_footer.scss';
 
 export const Footer = () => {
-  return <footer>Made with ❤️ and 🪄 by Bree Hall</footer>;
+  return (
+    <footer>
+      <p>Made with ❤️ and 🪄 by Bree Hall</p>
+      <p className="footer--disclaimer">
+        I do not own the rights to any properties or characters mentioned by the
+        generator. They solely belong to Disney.
+      </p>
+    </footer>
+  );
 };
